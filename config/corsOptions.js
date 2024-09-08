dccc87f5b64ca5cbbeb7ae2f219c836dd9697770
nhlamulomagwaza <magwazanhlamulo563@gmail.com>
2024-09-08 18:51:26 +0200

    debugged cors errors

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,19 +1,18 @@
-//Configuring cors
-
 const allowedOrigins = require('./allowedOrigins')
 
-const corsOptions = {
+const corsOptions = (res) => {
+  return {
     origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            res.header('Access-Control-Allow-Origin', origin)
-
-            callback(null, true)
-                    } else {
-            callback(new Error('Not allowed by CORS'))
-        }
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        res.header('Access-Control-Allow-Origin', origin)
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
     },
     credentials: true,
     optionsSuccessStatus: 200
+  }
 }
 
-module.exports = corsOptions 
+module.exports = corsOptions
